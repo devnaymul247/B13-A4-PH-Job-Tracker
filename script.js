c453ab9fb3fe051855e4e14aceb2ef070298a86b
Namul <namul@Namuls-MacBook-Air.local>
2026-02-24 05:43:02 +0600

    total and available job counter added

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ const totalCounter = document.getElementById('total-counter');
 const interviewCounter = document.getElementById('interview-counter');
 const rejectedCounter = document.getElementById('rejected-counter');
 
+// available-job-counter
+const availableJobCounter = document.getElementById('available-job-counter');
+
 const tab1 = document.querySelectorAll('#tab1 .card').length;
 totalCounter.innerText = tab1;
+availableJobCounter.innerText = tab1 + ' jobs';
+
